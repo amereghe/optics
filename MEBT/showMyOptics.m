fbe6985c7e0683fd59f1c42ad87b653906e06930
Alessio Mereghetti <amereghe@Ubuntu-20.04-LTS>
2021-05-24 14:10:24 +0200

    update template plotting scripts of HEBT and MEBT with plot of RMatrix

--- MEBT/showMyOptics.m
+++ MEBT/showMyOptics.m
@@ -6,12 +6,16 @@ pathToLibrary="..\..\MatLabTools";
 addpath(genpath(pathToLibrary));
 opticsFileName="example_optics.tfs";
 geometryFileName="example_geometry.tfs";
+rMatrixFileName="example_rmatrix.tfs";
 
 % acquire data
 optics = ParseTfsTable(opticsFileName,'optics');
 [Qx,Qy,Chrx,Chry,Laccel,headerNames,headerValues] = ...
     ParseTfsTableHeader(opticsFileName);
 geometry = ParseTfsTable(geometryFileName,'geometry');
+rMatrix = ParseTfsTable(rMatrixFileName,'rMatrix');
 
 % show the optics
-ShowOptics(optics,geometry,"MEBT - Protons",Laccel,Qx,Qy,Chrx,Chry);
+myTitle="MEBT - Protons";
+ShowOptics(optics,geometry,myTitle,Laccel,Qx,Qy,Chrx,Chry);
+ShowRmatrix(rMatrix,geometry,myTitle);
